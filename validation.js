6943202d26766d31eb6910f2b5ddd19aad0c56db
Eugenia Zapata <eugeniazapata@gmail.com>
2020-08-08 16:47:15 -0300

    My first landing page.
    HTML, CSS and JS ES6.

---
+++ validation.js
@@ -0,0 +1,23 @@
+const form = document.querySelector("#info")
+const email = document.querySelector("#email")
+const emailHelp = document.querySelector("#emailHelp")
+const password = document.querySelector("#password")
+const passwordHelp = document.querySelector("#passwordHelp")
+
+form.addEventListener("submit", (event) => {
+ event.preventDefault ()
+  if (email.value === "" || password.value === "") {
+    if (email.value === "") {
+        emailHelp.innerText = "Please try again"
+    }
+    if (password.value === "") {
+      passwordHelp.innerText = "Please try again"
+
+    }
+
+  } else {
+     form.submit();
+  }
+})
+
+
